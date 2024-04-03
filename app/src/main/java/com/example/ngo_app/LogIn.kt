07afd6fe0be64a510d2d256e7d85eda0789a967b
Sanjay <sanjaymali94284@gmail.com>
2024-04-04 00:25:55 +0530

    sharedPreferences added

--- app/src/main/java/com/example/ngo_app/LogIn.kt
+++ app/src/main/java/com/example/ngo_app/LogIn.kt
@@ -1,5 +1,6 @@
 package com.example.ngo_app
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
@@ -20,47 +21,17 @@ class LogIn : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.logInButton.setOnClickListener {
-            val phoneNumber = binding.email.text.toString()
+            val username = binding.uname.text.toString()
             val password = binding.password.text.toString()
 
-            if (phoneNumber.isEmpty() || password.isEmpty()){
+            if (username.isEmpty() || password.isEmpty()){
                 Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             else
             {
-                val myRef = database.getReference("Users")
-                myRef.child(phoneNumber).get().addOnSuccessListener {
-                    if (it.exists()){
-                        val user = it.child("phoneNumber").value
-                        myRef.child(phoneNumber).child("password").get().addOnSuccessListener { data ->
-                            if(it.exists())
-                            {
-                                val pwd = it.value
-                                if(pwd == password)
-                                {
-                                    val sIntent = Intent(this,MainActivity::class.java)
-                                    startActivity(sIntent)
-                                }
-                                else
-                                {
-                                    Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                            else
-                            {
-                                Toast.makeText(this, "Password not found", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
-                    }
-                }.addOnFailureListener {
-                    Toast.makeText(this, "Failure to load ", Toast.LENGTH_SHORT).show()
-                }
-
+                checkUser(username, password)
+                finish()
             }
 //                        if (data?.email == phoneNumber && data.password == password){
 //                            Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show()
@@ -74,18 +45,66 @@ class LogIn : AppCompatActivity() {
 //            }
         }
 
-        sharedPreferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE)
-        val isLoggedIn = sharedPreferences.getBoolean("IS_LOGGED_IN", false)
-        if (isLoggedIn){
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
+        if (isLoggedIn()) {
+            redirectToHome()
+        } else {
+            loginUser()
+        }
 
         binding.signUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity2::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun isLoggedIn(): Boolean {
+        return sharedPreferences.getBoolean("isLoggedIn", false)
+    }
+
+    private fun redirectToHome() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    private fun loginUser() {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isLoggedIn", true)
+        editor.apply()
+        redirectToHome()
+    }
+    private fun checkUser(username: String, password: String) {
+        val myRef = database.getReference("Users")
+        myRef.child(username).get().addOnSuccessListener {
+            if (it.exists()){
+                val user = it.child("username").value
+                myRef.child(username).child("password").get().addOnSuccessListener {
+                    if(it.exists())
+                    {
+                        val pwd = it.value
+                        if(pwd == password)
+                        {
+                            val sIntent = Intent(this,MainActivity::class.java)
+                            startActivity(sIntent)
+                        }
+                        else
+                        {
+                            Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "Password not found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failure to load ", Toast.LENGTH_SHORT).show()
         }
     }
 }
